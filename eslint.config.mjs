68eea2069a2fd785c6f1137f2a1492f7e4c44360
Matthew Bretz <matthew.bretz@edwardjones.com>
2026-02-05 14:52:53 -0600

    set up eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,32 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  // Custom overrides
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      // Integrate Prettier as ESLint rule
+      "prettier/prettier": [
+        "warn",
+        { singleQuote: true, semi: true, endOfLine: "auto" },
+      ],
+
+      // TypeScript tweaks
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+
+  // Override default ignores
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
   ]),
 ]);
-
-export default eslintConfig;
